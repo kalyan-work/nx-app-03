fd149261dbf01c326f105fbd91602d6ee23fba55
S Kalyan Chakravarthy <skachakr@publicisgroupe.net>
2022-08-15 19:57:36 +0530

    using atoms

--- libs/ui-react/src/lib/atoms/Button/Button.js
+++ libs/ui-react/src/lib/atoms/Button/Button.js
@@ -1,4 +1,9 @@
-import Button from '@mui/material/Button';
+import { default as MUIButton} from '@mui/material/Button';
 import styles from './Button.module.scss';
+
+export function Button(props) {
+    return (<div><MUIButton {...props}>{props.name || 'Click Me'}</MUIButton></div>);
+}
+;
 
 export default Button;
--- libs/ui-react/src/lib/atoms/Card/Card.js
+++ libs/ui-react/src/lib/atoms/Card/Card.js
@@ -1,4 +1,11 @@
-import styles from './Card.module.scss';
-import Card from '@mui/material/Card';
-  
+import styles from "./Card.module.scss";
+import { default as MUICard } from "@mui/material/Card";
+
+export function Card(props) {
+  return (
+    <div>
+      <MUICard {...props} />
+    </div>
+  );
+}
 export default Card;
