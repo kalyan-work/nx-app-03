da370d1b287d0f475a2c16e87609a54caf7a7bf4
S Kalyan Chakravarthy <skachakr@publicisgroupe.net>
2022-08-15 19:57:25 +0530

    Using atoms in app

--- apps/customers-app/src/app/app.tsx
+++ apps/customers-app/src/app/app.tsx
@@ -4,13 +4,14 @@ import NxWelcome from './nx-welcome';
 import { getFoo } from '../../../../libs/common-utils/src/';
 
 import { Route, Routes, Link } from 'react-router-dom';
-import { Title } from "@nx-app-03/ui-react";
+import { Title, Button } from "@nx-app-03/ui-react";
+
 export function App() {
   return (
     <>
       <div>{getFoo()}</div>
       <NxWelcome title="customers-app" />
-      <Title title="Hello" />
+      <Button variant="contained" name="Click Here" />
       <div />
 
       {/* START: routes */}
